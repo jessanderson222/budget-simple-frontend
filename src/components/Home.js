67944a260176e7660209066f342be8aa6fc1b965
jessanderson222 <jessanderson222@gmail.com>
2019-05-17 10:21:08 -0600

    successfully added material UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h3>home</h3>
+        <Button>Sign In</Button>
+        <Button>Create Account</Button>
         <button className="sign-in">Sign In</button>
         <button className="sign-in">Create Account</button>
       </div>
