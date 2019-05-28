85ff0a9cf625719ccf6f8b88247686ae5b646c6a
jessanderson222 <jessanderson222@gmail.com>
2019-05-28 11:49:24 -0600

    Yearly and monthly checkboxes work

--- src/components/BudgetForm.js
+++ src/components/BudgetForm.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 class BudgetForm extends Component {
   state = {
-    budget: ""
+    budget: "",
+    yearly: false,
+    monthly: false
   };
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  };
+  yearlyHandleChange = e => {
+    this.setState({
+      yearly: !this.state.yearly
+    });
+  };
+  monthlyHandleChange = e => {
+    this.setState({
+      monthly: !this.state.monthly
     });
   };
 
@@ -29,6 +43,24 @@ class BudgetForm extends Component {
             name="budget"
             onChange={this.handleChange}
           />
+          <br />
+          <FormControlLabel
+            value="yearly"
+            control={<Checkbox color="primary" />}
+            label="Yearly"
+            labelPlacement="end"
+            name="yearly"
+            onChange={e => this.yearlyHandleChange(e)}
+          />
+          <FormControlLabel
+            value="monthly"
+            control={<Checkbox color="primary" />}
+            label="Monthly"
+            labelPlacement="end"
+            name="monthly"
+            onChange={e => this.monthlyHandleChange(e)}
+          />
+
           <br />
           <button>Submit</button>
         </form>
